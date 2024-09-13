9ee45c51ab8ce3f8044fbb1d9a8f4e6d8eed8008
tathsaraniliyanage <prabodhathathsarani28@gmail.com>
2024-09-13 19:48:51 +0530

    java loops added

--- loopsjava.java
+++ loopsjava.java
@@ -1,5 +1,34 @@
 public class loopsjava{
     public static void main(String[] args){
+            apple();
+            grapes();
+            melon();
+            pera();
+            orange();
+    }
+
+    //for loop
+    public static void apple(){
+             
+    }
+
+    //for-each loop
+    public static void grapes(){
         
+    }
+
+    //nested loop
+    public static void melon(){
+      
+    }
+
+    //while loop
+    public static void pera(){
+
+    }
+
+    //do-while loop
+    public static void orange(){
+
     }
 }
