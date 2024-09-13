01dfa0baf1aa45cb52252af3e832880f9f96c892
tathsaraniliyanage <prabodhathathsarani28@gmail.com>
2024-09-13 14:35:08 +0530

    String concatenation updated

--- concatenation.java
+++ concatenation.java
@@ -1,3 +1,15 @@
-String firstName = "kamal";
-String lastName = "bimal";
-System.out.println(firstName + lastName)
+public class concatenation{
+    public static void main(String[] args){
+        number();
+    }
+     public static void number(){
+        String firstName = "kamal";
+        String lastName = "bimal";
+        System.out.println(firstName + " " + lastName);
+
+        String first = "John ";
+        String last = "Doe";
+        System.out.println(first.concat(last));
+     }
+}
+
