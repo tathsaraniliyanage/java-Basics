6c598a7afbef4b510673e9376d1e79cbfb78ed89
tathsaraniliyanage <prabodhathathsarani28@gmail.com>
2024-09-13 09:11:48 +0530

    project init

---
+++ main.java
@@ -0,0 +1,18 @@
+class main{
+    public static void main(String[] args){
+        mango();
+        System.out.println("Hello Java");
+        apple();
+        number(10,20);
+    }
+
+    public static void mango(){
+        System.out.println("my mango");
+    }
+
+    public static void apple(){
+        System.out.println("my apple");
+    }
+
+
+}
