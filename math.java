254840e7314618f8cd02666813ace458117baffa
tathsaraniliyanage <prabodhathathsarani28@gmail.com>
2024-09-13 14:54:30 +0530

    java math added

---
+++ math.java
@@ -0,0 +1,30 @@
+public class math{
+    public static void main(String[] args){
+            Hello();
+    }
+    public static void Hello(){
+           int a = Math.max(5,10);
+           System.out.println(a);
+
+           int b = Math.min(100,200);
+           System.out.println(b);
+
+           Double c = Math.sqrt(60);
+           Double d = Math.sqrt(64);
+           System.out.println(c);
+           System.out.println(d);
+    }
+}
+
+
+
+// Java Math
+
+// Math.max(x,y)
+// The Math.max(x,y) method can be used to find the highest value of x and y
+
+// Math.min(x,y)
+// The Math.min(x,y) method can be used to find the lowest value of x and y
+
+// Math.sqrt(x)
+// The Math.sqrt(x) method returns the square root of x
