f68793820056aea4866d4a0a7b6284ca72cec564
tathsaraniliyanage <prabodhathathsarani28@gmail.com>
2024-09-13 14:45:39 +0530

    String special characters updated

---
+++ numbersandstrings.java
@@ -0,0 +1,20 @@
+public class numbersandstrings{
+    public static void main(String[] args){
+            characters();
+    }
+    public static void characters(){
+            String txt = "dhfd ndfdfhds  \n dfdhf fdfdsgggghh \t dfdsdffddfs fddsfA \b Bfdgfgg fgffsd \f fgfgdffd fdfgdgdggdcsfd \r fdfsdf gfdfsdfs ";
+            System.out.println(txt);
+    }
+
+}
+
+
+
+// Strings - Special Characters
+
+// \n	New Line	
+// \r	Carriage Return	
+// \t	Tab	
+// \b	Backspace	
+// \f	Form Feed	
